b4d4b45443cc3d2de8939347c31f99faab0002c3
Andreas Perhab <bigbear.ap@gmx.at>
2014-02-11 11:24:58 +0100

    set height and width for general layout to fill the screen

--- src/com/dc2f/cms/gui/Dc2fUi.java
+++ src/com/dc2f/cms/gui/Dc2fUi.java
@@ -6,11 +6,9 @@ import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
-import com.vaadin.ui.Button;
-import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.HorizontalSplitPanel;
 import com.vaadin.ui.Label;
-import com.vaadin.ui.Tree;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
 
@@ -28,8 +26,11 @@ public class Dc2fUi extends UI {
 		
 		final VerticalLayout layout = new VerticalLayout();
 		setContent(layout);
-		final HorizontalLayout main = new HorizontalLayout();
+		layout.setHeight(100, Unit.PERCENTAGE);
+		final HorizontalSplitPanel main = new HorizontalSplitPanel();
 		layout.addComponent(main);
+		main.setHeight(100, Unit.PERCENTAGE);
+		main.setSplitPosition(10, Unit.PERCENTAGE);
 		
 		final Dc2fTree tree = new Dc2fTree();
 		final Label filelist = new Label("filelist");
