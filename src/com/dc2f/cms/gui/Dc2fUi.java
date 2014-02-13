9daf7ad55c091f312728f05ef242512f9f1db830
Andreas Perhab <bigbear.ap@gmx.at>
2014-02-13 16:36:52 +0100

    rearranged design so the first 2 columns use 200 pixels each

--- src/com/dc2f/cms/gui/Dc2fUi.java
+++ src/com/dc2f/cms/gui/Dc2fUi.java
@@ -31,18 +31,20 @@ public class Dc2fUi extends UI {
 		final HorizontalSplitPanel main = new HorizontalSplitPanel();
 		layout.addComponent(main);
 		main.setHeight(100, Unit.PERCENTAGE);
-		main.setSplitPosition(10, Unit.PERCENTAGE);
+		main.setSplitPosition(200, Unit.PIXELS);
 		
 		final Dc2fTree tree = new Dc2fTree();
-		final Dc2fFileList filelist = new Dc2fFileList();
-		filelist.setHeight(100, Unit.PERCENTAGE);
-		final Dc2fEditor editor = new Dc2fEditor();
 		main.addComponent(tree);
-		final HorizontalLayout right = new HorizontalLayout();
+		final HorizontalSplitPanel right = new HorizontalSplitPanel();
 		main.addComponent(right);
 		right.setHeight(100, Unit.PERCENTAGE);
 		right.setWidth(100, Unit.PERCENTAGE);
+		right.setSplitPosition(200, Unit.PIXELS);
+		
+		final Dc2fFileList filelist = new Dc2fFileList();
+		filelist.setHeight(100, Unit.PERCENTAGE);
 		right.addComponent(filelist);
+		final Dc2fEditor editor = new Dc2fEditor();
 		right.addComponent(editor);
 		
 		final Dc2fNavigationClickListener clickListener = new Dc2fNavigationClickListener(tree, filelist, editor);
